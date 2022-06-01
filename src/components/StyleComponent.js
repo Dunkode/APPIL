623c39fc0cad14459d0fe823b2654c6b4768b4bd
Ederson <edersonvidal137@gmail.com>
2022-06-01 13:22:38 -0300

    Confecção da tela de Login

---
+++ src/components/StyleComponent.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#b2dad2',
+            alignItems: 'center',
+            justifyContent: 'center',
+    
+        },
+        textInput: {
+            backgroundColor: '#d2ece6',
+            margin: 5,
+            width: "60%",
+            padding: 3,
+            borderRadius: 5,
+            width: '70%',
+            // height: '60%'
+        }
+    })
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,31 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Styles from '../components/StyleComponent'
+import { TextInput } from 'react-native'
 
 export default function Login() {
   return (
-    <View>
+    <View style={Styles.container}>
       <Text>Login</Text>
+      
+      <View>
+        <TextInput
+          placeholder='E-mail'
+          style={Styles.textInput}
+        >
+
+        </TextInput>
+        
+      </View>
+      
+      <View>
+        <TextInput
+          placeholder='Senha'
+          style={Styles.textInput}
+        >
+        </TextInput>
+        
+      </View>
     </View>
   )
 }
