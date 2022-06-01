64a7db3b5e9a5eb348187b79b61eab9179c14feb
Ederson <edersonvidal137@gmail.com>
2022-05-31 22:26:42 -0300

    Montagem da estrutura inicial das telas

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './src/screens/Login';
+import Sobre from './src/screens/Sobre';
+import CadastroUsuario from './src/screens/CadastroUsuario';
+import ListaFarmacias from './src/screens/ListaFarmacias';
+import ListaRemedios from './src/screens/ListaRemedios';
+import PerguntasFrequentes from './src/screens/PerguntasFrequentes';
 
 export default function App() {
+  const Stack = createNativeStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='Login'
+      >
+
+        <Stack.Screen
+          name='Login'
+          component={Login}
+          options={
+            { title: "Login" }
+          }
+        />
+
+        <Stack.Screen
+          name='CadastroUsuario'
+          component={CadastroUsuario}
+          options={
+            { title: "Cadastrar-se" }
+          }
+        />
+
+        <Stack.Screen
+          name='ListaRemedios'
+          component={ListaRemedios}
+          options={
+            { title: "Remédios" }
+          }
+        />
+
+        <Stack.Screen
+          name='ListaFarmacias'
+          component={ListaFarmacias}
+          options={
+            { title: "ListaFarmacias" }
+          }
+        />
+
+        <Stack.Screen
+          name='PerguntasFrequentes'
+          component={PerguntasFrequentes}
+          options={
+            { title: "PerguntasFrequentes" }
+          }
+        />
+
+        <Stack.Screen
+          name='Sobre'
+          component={Sobre}
+          options={
+            { title: "Sobre" }
+          }
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/ListaFarmacias.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ListaFarmacias() {
+  return (
+    <View>
+      <Text>ListaFarmacias</Text>
+    </View>
+  )
+}
